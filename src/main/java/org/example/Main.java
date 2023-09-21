0d5ab9695b19551757586a8ebcefc805eb9541ca
Max <private.kuehn@protonmail.com>
2023-09-21 17:16:07 +0200

    Refactored to enable writing of the entire dataset. Since the foreign keys are NOT NULL fields a reading method is necessary for the code to be functional.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,10 +10,10 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-import static java.sql.DriverManager.println;
 
 public class Main {
     public static void main(String[] args) {
+
         try {
             ObjectMapper objectMapper = new ObjectMapper();
             JsonNode rootNode = objectMapper.readTree(new File("data.json"));
@@ -23,22 +23,72 @@ public class Main {
             String password = "Frosted-Barrette4-Revisable";
 
             Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
-            String sql = "INSERT INTO phone (type, number, user_id) VALUES (?, ?, ?)";
+
+            write_data(connection, rootNode);
+
+        } catch (SQLException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void write_data(Connection connection, JsonNode rootNode) {
+        // check if address already exists. If not write address data
+        write_address_data(connection, rootNode.get("address"));
+        Integer address_id = 1;  // Rewrite using a reading method
+        write_user_data(connection, rootNode.get("user"), address_id);
+        Integer user_id = 1;  // Rewrite using a reading method
+        write_phone_data(connection, rootNode.get("phoneNumber"), user_id);
+    }
+
+    public static void write_address_data(Connection connection, JsonNode address_node) {
+        try {
+            String sql = "INSERT INTO address (streetAddress, city, state, postalCode) VALUES (?, ?, ?, ?)";
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
 
-            // preparedStatement.setString(1, rootNode.get("phoneNumber").get(0).get("type").asText());
-            // preparedStatement.setString(2, rootNode.get("phoneNumber").get(0).get("number").asText());
-            // preparedStatement.setString(3, "1");
+            preparedStatement.setString(1, address_node.get("streetAddress").asText());
+            preparedStatement.setString(2, address_node.get("city").asText());
+            preparedStatement.setString(3, address_node.get("state").asText());
+            preparedStatement.setInt(4, address_node.get("postalCode").asInt());
 
-            preparedStatement.setString(1, rootNode.get("phoneNumber").get(1).get("type").asText());
-            preparedStatement.setString(2, rootNode.get("phoneNumber").get(1).get("number").asText());
-            preparedStatement.setString(3, "1");
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void write_user_data(Connection connection, JsonNode rootNode, Integer address_id ) {
+        try {
+            String sql = "INSERT INTO user (firstName, lastName, age, address_id) VALUES (?, ?, ?, ?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+            preparedStatement.setString(1, rootNode.get("firstName").asText());
+            preparedStatement.setString(2, rootNode.get("lastName").asText());
+            preparedStatement.setString(2, rootNode.get("lastName").asText());
+            preparedStatement.setInt(4, address_id);
 
             preparedStatement.executeUpdate();
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
-        } catch (IOException e) {
+        }
+    }
+
+    private static void write_phone_data(Connection connection, JsonNode phone_node, Integer user_id) {
+        try {
+            String sql = "INSERT INTO phone (type, number, user_id) VALUES (?, ?, ?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+            // Refactor to loop over all phone entries instead
+            preparedStatement.setString(1, phone_node.get(0).get("type").asText());
+            preparedStatement.setString(2, phone_node.get(0).get("number").asText());
+            preparedStatement.setInt(3, user_id);
+
+            preparedStatement.setString(1, phone_node.get(1).get("type").asText());
+            preparedStatement.setString(2, phone_node.get(1).get("number").asText());
+            preparedStatement.setInt(3, user_id);
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
